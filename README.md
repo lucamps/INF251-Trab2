90bcb1936b54ccc269b557347ff10fa5a983a8e1
lucamps <lucas.campos2@ufv.br>
2019-09-01 14:36:44 -0300

    Add parte 2 em baixo nivel & update README

--- README.md
+++ README.md
@@ -10,4 +10,8 @@ Trabalho 2  da disciplina INF251(Organização de Computadores 1) - UFV 2019/2
 <ul>
 <b>Parte 2</b>
   <li>Alto nivel - ok</li>
+  <li>Baixo nível - Ok</li>
+  <li>Desenho e calcular - Em andamento</li>
+  <li>Responder Perguntas - Ainda não</li>
+  <li>Criar testbench - Ainda não</li>
 </ul>
--- parte2.v
+++ parte2.v
@@ -1,9 +1,10 @@
 /*
-********** Alu 2 ************** | 
-c1 0  c0 0      A+B             |
-c1 0  c0 1      A+B+1           |
-c1 1  c0 0      A&B             |
-c1 1  c0 1      A^B             |
+******************* Alu 2 *****************| 
+c1    c0               d1  d2  d3  d4  d5  |
+ 0     0      A+B       x   x   x   0   1  |
+ 0     1      A+B+1     x   x   x   1   1  |
+ 1     0      A&B       1   0   0   x   0  |
+ 1     1      A^B       0   1   1   x   0  |
 */
 
 
@@ -14,18 +15,26 @@ module alu2_altonivel(input [3:0] a,b, input [1:0] c, output [3:0] F);
   assign F1 = ( c[0]  )? a^b : a&b ;
 endmodule 
 
-/*module alu2(input [3:0] a,b, input [1:0] c, output [3:0] F);  //Errado
-  wire [3:0] And, Add;
-  wire d1,d2;
+module alu2(input [3:0] a,b, input [1:0] c, output [3:0] F);
+  wire [3:0] And1, And2, And3, And4, Xor1, Xor2, Add;
+  wire d1,d2,d3,d4,d5;
 
-  assign d1 = c[0] & ~c[1];
-  assign d2 = (~c[0] & c[1]); 
+  assign d1 = ~c[0];
+  assign d2 = c[0]; 
+  assign d3 = c[0];
+  assign d4 = c[0];
+  assign d5 = ~c[1];
 
-  assign And = a & b;
-  assign Add = a + b + d1;
-  assign F = (d2)?Add:And;
+  assign And1 = a & And2;
+  assign And2 = b & {4{d1}};
+  assign And3 = {4{d2}} & a;
+  assign And4 = {4{d3}} & b;
+  assign Xor1 = And1 ^ Xor2;
+  assign Xor2 = And4 ^ And3;
+  assign Add = a + b + d4;
+  assign F = (d5)?Add:Xor1;
 
-endmodule */
+endmodule
 
 module testbench();
    
